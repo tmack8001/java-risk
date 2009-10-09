86696a1d8f5e71b94d1965cfce348b71a5019eed
dehall622 <dehall622@b0351cfc-b502-11de-9fe4-7baf7ef90cf5>
2009-10-09 19:03:11 +0000

    added Opcodes

--- src/javaRisk/Constants.java
+++ src/javaRisk/Constants.java
@@ -4,4 +4,25 @@ public final class Constants {
 
 	public static final int PORT = 1988;
 	
+	// OPCODES
+	
+	public static final byte READY = 1;
+	
+	// ATTACK -- args: src-territory, dest-territory
+	public static final byte ATTACK = 2;
+	
+	public static final byte SURRENDER = 13;
+	
+	public static final byte END_TURN = 4;
+	
+	public static final byte GAME_STARTING = 5;
+	
+	// TURN_IND -- args: player-num
+	public static final byte TURN_IND = 6;
+	
+	// ATTACK_MADE -- args: src-territory, dest-territory
+	public static final byte ATTACK_MADE = 7;
+	
+	public static final byte TERRITORY_STATUS = 8;
+	
 }
