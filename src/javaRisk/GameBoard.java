6426934e8f2ed2d22de02344fd1e88a3a0470907
Drummer8001 <Drummer8001@b0351cfc-b502-11de-9fe4-7baf7ef90cf5>
2009-10-16 22:41:02 +0000

    initial go at GameBoard (Model)

--- src/javaRisk/GameBoard.java
+++ src/javaRisk/GameBoard.java
@@ -1,16 +1,59 @@
-package javaRisk;
 /**
- * 
+ * GameBoard.java
  */
+package javaRisk;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
+ * Representation of a general Risk Game.
+ * 
  * @author Trevor Mack
- *
+ * 
  */
 public class GameBoard {
 	
-	public GameBoard() {
-		
+	private ArrayList<Territory> territories;
+	private ArrayList<Player> players;
+	private int currentMove = 0;
+	
+	private static final int NUM_ARMY = 40;
+	
+	public GameBoard(ArrayList<Player> players, int rows, int cols) {
+		this.players = players;
+		initializeBoard(rows, cols);
 	}
 	
+	public void initializeBoard(int rows, int cols) {
+		int index = 0;
+		for(int row=0; row<rows; row++) {
+			for(int col=0; col<cols; col++) {
+				territories.add(index, new Territory(index, row, col));
+			}
+		}
+	}
+	
+	public void updateTerritory(int index, int owner, int size) {
+		Territory territory = territories.get( index );
+		territory.setArmy( new Army(players.get(owner), size) );
+	}
+	
+	public boolean winnerExists() {
+		Iterator<Player> iterator = players.iterator();
+		int numAlive = 0;
+		while( iterator.hasNext() ) {
+			if(iterator.next().isAlive()) {
+				numAlive++;
+			}
+			if(numAlive > 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public int getCurrentMove() {
+		return currentMove;
+	}
 }
